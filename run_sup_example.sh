da9d515ac2c87640b5c2dbbcd71ca14f93a4ca54
CassDurr <c.durr@instadeep.com>
2022-11-30 14:10:42 +0200

    Change forked branch

--- run_sup_example.sh
+++ run_sup_example.sh
@@ -14,23 +14,23 @@ export OMP_NUM_THREADS=8
 
 # Use distributed data parallel
 # If you only want to use one card, uncomment the following line and comment the line with "torch.distributed.launch"
-# python train.py \
-python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
+# python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
+#    --do_eval \
+#    --eval_steps 125 \
+#    --evaluation_strategy steps \
+python train.py \
     --model_name_or_path bert-base-uncased \
-    --train_file data/nli_for_simcse.csv \
-    --output_dir result/my-sup-simcse-bert-base-uncased \
-    --num_train_epochs 3 \
+    --train_file data/sample_peptides_triplet_new.csv \
+    --output_dir result/sample_peptides_triplet-bert-base-uncased \
+    --num_train_epochs 100 \
     --per_device_train_batch_size 128 \
     --learning_rate 5e-5 \
-    --max_seq_length 32 \
-    --evaluation_strategy steps \
+    --max_seq_length 15 \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
-    --eval_steps 125 \
     --pooler_type cls \
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --do_eval \
     --fp16 \
     "$@"
